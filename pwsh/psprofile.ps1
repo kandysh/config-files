53fcd5b3353a48cff59b94c80376a7a612d0d43e
kandysh <rajawat.k.029@gmail.com>
2024-07-10 21:13:33 +0530

    updated the ps profile

--- pwsh/psprofile.ps1
+++ pwsh/psprofile.ps1
@@ -1,9 +1,9 @@
-$path = "C:\Users\kanda\Local\Programs\oh-my-posh\themes"
-$themes = Get-Children -Path $path -File -Recurse -Filter "*omp.json"
+$path = "C:\Users\kanda\AppData\Local\Programs\oh-my-posh\themes"
+$themes = Get-ChildItem -Path $path -File -Recurse -Filter "*omp.json"
 $selectedtheme = $themes | Get-Random
 oh-my-posh init pwsh --config $selectedtheme | Invoke-Expression
 Import-Module -Name Terminal-Icons
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-Write-Host (Get-Items $selectedtheme).Basename
+Write-Host (Get-Item $selectedtheme).Basename
 Import-Module -Name Terminal-Icons
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
